76e000a30c69308918d7bfc7a009991115624930
ChunTian Liu <lcntn3@gmail.com>
2014-05-13 16:13:23 +0800

    beautify

--- pca.py
+++ pca.py
@@ -1,47 +1,30 @@
 import numpy as np
 from scipy.sparse.linalg import eigs
 
+# TODO
+# It's seems that k should be only can the situations that when k less or
+# equal than rank(cov) -1.
+# Find other alternatives or delve more details into eigs()
+
 def pca(X, k):
   """ Compute the principal components analysis of matrix X, only the first k
       eigenvalues and eigenvectors will be returned.
 
-  Parmaters
-  ---------
-  X: matrix
-     A feature matrix with shape (n, d)
-
-  k: matrix
-     The number of principal components need to be returned 
-
-  Returns
-  -------
-  eigvec: k eigenvectors
-          An matrix with shape (d, k)
-
-  eigval: k eigenvalues 
-          An matrix with shape (1, k)
+  Args:
+    X: matrix (n x d). A feature matrix
+    k: integer. The number of principal components need to be returned 
+  Returns:
+    eigvec: k eigenvectors. An matrix with shape (d, k)
+    eigval: k eigenvalues. An matrix with shape (1, k)
+    Note: the eigvec and eigval is corresponding, however, the eigval are not
+          sorted, which likes the `eigs()` version of matlab
   """
   n, dim = X.shape
 
-  # center the data
+  # Center the data
   X_mean = np.mean(X, axis = 0)
   X = X - X_mean
-  # get the covariance matrix
+  # Get the covariance matrix
   covariance_matrix = np.dot(X.T, X) / (n - 1)
   eigval, eigvec = eigs(covariance_matrix, k)
   return np.array(eigvec), np.array(eigval)
-
-#X = [
-    #[2.5, 2.4, 4.5],
-    #[0.5, 0.7, 4.5],
-    #[2.2, 2.9, 9.3],
-    #[1.9, 2.2, 2.3],
-    #[3.1, 3.0, 2.1],
-    #[2.3, 2.7, 5.3],
-    #[2.0, 1.6, 2.9],
-    #[1.0, 1.1, 9.4],
-    #[1.5, 1.6, 8.1],
-    #[1.1, 0.9, 4.2],
-    #];
-#X = np.array(X);
-#pca(X)
