6292b33516e45dbf5b5959c8cc1de63d8e6ae60b
ChunTian Liu <lcntn3@gmail.com>
2014-05-19 17:12:30 +0800

    delete files

--- tests/test_compactbit.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from ../compactbit import *
-
-class CompactbitTestCase(unittest.TestCase):
-  """ Test for 'compactbit.py' """
-
-if __name__ == '__main__':
-  unittest.main()
--- tests/test_precision_recall.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from ../precision_recall import *
-
-class PrecisionRecallTestCase(unittest.TestCase):
-  """ Test for 'precision_recall.py' """
-
-if __name__ == '__main__':
-  unittest.main()
